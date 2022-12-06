29381f9bc5f7c38993596e6a93dc4eeac64d24f0
MD Golam Rabbi <rabbi.kaunia@gmail.com>
2022-12-06 19:30:41 +0600

    fghhasd

--- personal_app/models.py
+++ personal_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 
 # Create your models here.
 class Interests(models.Model):
@@ -12,10 +13,17 @@ class Interests(models.Model):
 class Services(models.Model):
     title = models.CharField(max_length = 150)
     icon = models.CharField(max_length = 150)
-    description = models.TextField()
+    description = RichTextField(default='xyz')
                
     def __str__(self):
         return self.title
+    
+class Testmonials(models.Model):
+    icon1 = models.CharField(max_length=50)
+    description = models.ForeignKey(Services, on_delete=models.CASCADE)    
+    profile = models.ImageField(upload_to='ProductImage')
+    
+    
     
 class Contact(models.Model):
     name = models.CharField(max_length = 150)
